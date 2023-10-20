5437c917e5387ce54f534fbdb4019e25fda47f3e
ronal <ronaldmiguel94@gmail.com>
2023-10-20 13:16:49 -0500

    agregue las entidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+import { ModerationActionComponent } from './moderation-action/moderation-action.component';
+import { ProfessorComponent } from './professor/professor.component';
+import { ReviewComponent } from './review/review.component';
+import { ReviewComentComponent } from './review-coment/review-coment.component';
+import { RoleComponent } from './role/role.component';
+import { UniversityComponent } from './university/university.component';
+import { UserAppComponent } from './user-app/user-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent,
+    ModerationActionComponent,
+    ProfessorComponent,
+    ReviewComponent,
+    ReviewComentComponent,
+    RoleComponent,
+    UniversityComponent,
+    UserAppComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/moderation-action/moderation-action.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-moderation-action',
+  templateUrl: './moderation-action.component.html',
+  styleUrls: ['./moderation-action.component.css']
+})
+export class ModerationActionComponent {
+
+}
