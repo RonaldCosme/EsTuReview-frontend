cc43dff4bc8e56dcebe8e61b558b56bebb44955b
RonaldCosme <u20192018@upc.edu.pe>
2023-11-07 21:28:13 -0500

    cambios

---
+++ src/app/components/course/naw-all-insert-course/naw-all-insert-course.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-naw-all-insert-course',
+  templateUrl: './naw-all-insert-course.component.html',
+  styleUrls: ['./naw-all-insert-course.component.css']
+})
+export class NawAllInsertCourseComponent {
+
+}
