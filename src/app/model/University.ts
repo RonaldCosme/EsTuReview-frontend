a182384d84b0c18ddfce7ad68b448b0896a63b77
RonaldCosme <72050980+RonaldCosme@users.noreply.github.com>
2023-11-13 22:23:25 -0500

    Delete src directory

--- src/app/model/University.ts
+++
@@ -1,8 +0,0 @@
-export class University {
-    universityId: number = 0;
-    name: string = '';
-    campus: string = '';
-    location: string = '';
-    description: string = '';
-  }
-  
--- src/app/model/UserApp.ts
+++
@@ -1,9 +0,0 @@
-export class UserApp {
-    userappId: number = 0;
-    name: string = '';
-    email: string = '';
-    registrationDate: Date = new Date(Date.now());
-    role: string = '';
-    universityId: number = 0;
-  }
-  
